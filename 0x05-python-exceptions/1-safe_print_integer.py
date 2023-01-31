f16cf823193620e8d5cebbaa38f29100ddfad211
C1LA <amondicheche@gmail.com>
2023-01-31 00:17:32 +0000

    Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-safe_print_integer(value):
+def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except(ValueError, TypeError):
-        return False
-    else:
-        return True
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
