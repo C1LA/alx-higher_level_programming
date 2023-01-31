a1dc5e4c247637f01ff1f67dab9f76d8745d9fcb
C1LA <amondicheche@gmail.com>
2023-01-31 00:08:35 +0000

    Exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except(ValueError, TypeError):
+        return False
+    else:
+        return True
