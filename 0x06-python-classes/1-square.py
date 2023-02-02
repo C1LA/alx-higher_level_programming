21c3cd1675cfc9c10bfb4146f73dc04d2d0c990a
C1LA <amondicheche@gmail.com>
2023-02-02 22:34:13 +0000

    Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Square class definition"""
 
+
 class Square:
     """Represents a square
     Attributes:
@@ -12,4 +13,4 @@ class Square:
             size (int): size of a side of the square
         Returns: None
         """
-        self.__size = 
+        self.__size = size
